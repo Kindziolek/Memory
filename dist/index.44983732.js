32f0a53184e5264783c7433ee8e7817b3e7d501a
Kindziolek <kingapietron@gmail.com>
2023-03-02 23:06:27 +0100

    memory v2

---
+++ dist/index.44983732.js
@@ -0,0 +1,30 @@
+/*
+
+generowanie 24 kart (6x4)
+przypisywanie 12 obrazków w losowej kolejności x2
+
+po kliknięciu element się obraca/odkrywa
+maksymalnie można mieć odkryte dwie karty
+przy kliknięciu w trzecią kartę, dwie pierwsze się zakrywają i odkrywa się trzecia
+przy odkryciu dwóch takich samych kart - zostają odkryte na stałe lub usunięte
+zliczanie liczby klików
+mierzenie czasu od pierwszego kliknięcia
+
+*/ const memoryGame = {} // const cells = document.querySelectorAll(".cell");
+ // const game = [];
+ // const generateGame = () => [];
+ // cells.forEach((cell) => {
+ //   cell.addEventListener("mouseenter", () => {
+ //     cell.style.border = "2px solid #FFFFFE";
+ //   });
+ //   cell.addEventListener("mouseleave", () => {
+ //     cell.style.border = "";
+ //   });
+ //   cell.addEventListener("click", () => {
+ //       cell.classList.toggle("on");
+ //     console.log("click");
+ //   });
+ // });
+;
+
+//# sourceMappingURL=index.44983732.js.map
