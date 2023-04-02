b87854f056503fa9e6e0430c5cba1e85a05da087
Kindziolek <kingapietron@gmail.com>
2023-04-02 23:08:40 +0200

    Img add

--- src/script.js
+++ src/script.js
@@ -9,6 +9,16 @@ let li = document.createElement("li");
 
 let images = ["PM-1.png", "PM-2.png", "PM-3.png", "PM-4.png", "PM-5.png", "PM-6.png", "PM-7.png", "PM-8.png", "PM-9.png", "PM-10.png", "PM-11.png", "PM-12.png"]
 
+images.forEach((img) => {
+    let li = document.createElement("li");
+    let element = document.createElement("img");
+    element.src='./images/'+img
+    grid.append(li)
+    li.append(element);
+    li.classList="cell";
+    element.classList="cell_img"
+})
+
 console.log(images)
 
 // function createCell() {
