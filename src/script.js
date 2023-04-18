2b6699cfdd20055cba48fc6d2cddc0ff99d6f00d
Kindziolek <kingapietron@gmail.com>
2023-04-18 23:49:38 +0200

    css

--- src/script.js
+++ src/script.js
@@ -1,60 +1,56 @@
 const plants = [
-    "PM-1.png",
-    "PM-2.png",
-    "PM-3.png",
-    "PM-4.png",
-    "PM-5.png",
-    "PM-6.png",
-    "PM-7.png",
-    "PM-8.png",
-    "PM-9.png",
-    "PM-10.png",
-    // "PM-11.png",
-    // "PM-12.png",
-    "PM-1.png",
-    "PM-2.png",
-    "PM-3.png",
-    "PM-4.png",
-    "PM-5.png",
-    "PM-6.png",
-    "PM-7.png",
-    "PM-8.png",
-    "PM-9.png",
-    "PM-10.png",
-    // "PM-11.png",
-    // "PM-12.png",
-  ];
-  
-  let click = []
-  
-  let startBtn = document.querySelector("#startGame");
-  let grid = document.querySelector("#grid");
-  let cellFront = document.querySelector(".cell_front");
-  let timer = document.querySelector("#timer");
-  let clicks = document.querySelector("#clicks");
-  
+  "PM-1.png",
+  "PM-2.png",
+  "PM-3.png",
+  "PM-4.png",
+  "PM-5.png",
+  "PM-6.png",
+  "PM-7.png",
+  "PM-8.png",
+  "PM-9.png",
+  "PM-10.png",
+  // "PM-11.png",
+  // "PM-12.png",
+  "PM-1.png",
+  "PM-2.png",
+  "PM-3.png",
+  "PM-4.png",
+  "PM-5.png",
+  "PM-6.png",
+  "PM-7.png",
+  "PM-8.png",
+  "PM-9.png",
+  "PM-10.png",
+  // "PM-11.png",
+  // "PM-12.png",
+];
 
-  let totalTime = 0;
+let click = [];
 
-  totalTime = setInterval(() => {
-    totalTime++
-    timer.innerHTML=`Time: ${totalTime} sec`
+let startBtn = document.querySelector("#startGame");
+let grid = document.querySelector("#grid");
+let cellFront = document.querySelector(".cell_front");
+let timer = document.querySelector("#timer");
+let clicks = document.querySelector("#clicks");
+
+let totalTime = 0;
+
+totalTime = setInterval(() => {
+  totalTime++;
+  timer.innerHTML = `Time: ${totalTime} sec`;
 }, 1000);
 
-  startBtn.addEventListener("click", () => {
-    grid.innerHTML = "";
-    flippCell = 0;
-    clicks.innerHTML=`Clicks:`
-    totalTime = 0;
-    click = [];
-    startGame();
-  });
-  
-  function startGame() {
-  
-    
-    totalTime = 0,
+startBtn.addEventListener("click", () => {
+  grid.innerHTML = "";
+  flippCell = 0;
+  clicks.innerHTML = `Clicks:`;
+  totalTime = 0;
+  click = [];
+  startGame();
+});
 
+function startGame() {
+  (totalTime = 0),
     plants
       .map((value) => ({
         value,
@@ -71,45 +67,37 @@ const plants = [
         element.classList = "cell_img";
         element.style.visibility = "hidden";
         li.classList = "cell_front";
-  
+
         let cellBack = document.createElement("img");
         cellBack.src = "./images/cell-background.png";
-  
+
         li.append(cellBack);
         cellBack.classList = "cell_back";
-  
+
         li.addEventListener("mouseenter", () => {
           li.style.border = "2px solid #FFFFFE";
         });
         li.addEventListener("mouseleave", () => {
           li.style.border = "";
         });
-  
-  
-  
-  
+
         li.addEventListener("click", (el) => {
-       
-  
           let front = el.target.parentNode.querySelector(".cell_img");
           let back = el.target.parentNode.querySelector(".cell_back");
-  
-         
-  
+
           if ((front.style.visibility = "hidden")) {
             front.style.visibility = "visible";
             back.style.visibility = "hidden";
             front.classList.add("cell_img_flipped");
           }
           const flipp = grid.querySelectorAll(".cell_img_flipped");
-  
+
           let flippCell = flipp.length;
-          
-          click.push(flippCell)
+
+          click.push(flippCell);
           clicksCell = click.length;
-          clicks.innerHTML=`Clicks: ${clicksCell}`
-  
-        
+          clicks.innerHTML = `Clicks: ${clicksCell}`;
+
           if (flippCell === 2) {
             if (flipp[0].src === flipp[1].src) {
               [...flipp].map((v) => {
@@ -133,7 +121,6 @@ const plants = [
           }
         });
       });
-  }
-  
-  startGame();
-  
+}
+
+startGame();
